33b45a6c37eadb2ec5f65f4482eae9d613178a27
Ivan <iFang.py@gmail.com>
2023-07-01 17:39:53 +1000

    e

--- src/controllers/suggest.js
+++ src/controllers/suggest.js
@@ -2,11 +2,12 @@
 // TO-DO: API for live events (get the data from the API---a Python child -> predictHQ)
 // TO-DO: Suggest places and events off of top genres
 
+const { exec } = require('node:child_process')
 // Calculating places based of genres and lists of places
 function suggest(genre, placeArr, eventsArr) {
     // Match genre to types of place
-    // places = placeArr[place][placeGenreMatch]
-    // events = eventsArr[place][placeGenreMatch]
+    places = exec('"/places.py"');
+    events = exec('"/events.py"');
     return {
         places: places,
         events: events,
