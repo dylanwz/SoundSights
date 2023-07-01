9efed0e64e51bd7bc90fd70fe3a90d95d657b07c
Ivan <iFang.py@gmail.com>
2023-07-01 17:32:27 +1000

    first iter of retrieving events/places

--- src/controllers/suggest.js
+++ src/controllers/suggest.js
@@ -1,5 +1,14 @@
 // TO-DO: Convert raw spotify data to list of top genres
-// TO-DO: API for live events (get the data from the API---a Python child)
+// TO-DO: API for live events (get the data from the API---a Python child -> predictHQ)
 // TO-DO: Suggest places and events off of top genres
 
-function suggest(gen)
+// Calculating places based of genres and lists of places
+function suggest(genre, placeArr, eventsArr) {
+    // Match genre to types of place
+    // places = placeArr[place][placeGenreMatch]
+    // events = eventsArr[place][placeGenreMatch]
+    return {
+        places: places,
+        events: events,
+    }
+}
