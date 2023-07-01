2092fb61eb479d051f26083410e276902a2bcf06
Ivan <iFang.py@gmail.com>
2023-07-01 16:17:32 +1000

    e

---
+++ src/controllers/suggest.js
@@ -0,0 +1,5 @@
+// TO-DO: Convert raw spotify data to list of top genres
+// TO-DO: API for live events (get the data from the API---a Python child)
+// TO-DO: Suggest places and events off of top genres
+
+function suggest(gen)
