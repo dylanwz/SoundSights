493fa41fb3c2211834a52c510259b4e7e97d8c7b
John Sakoutis <johnpsakoutis@gmail.com>
2023-07-01 16:04:32 +1000

    e

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// var fs = require('fs');
+var http = require('http');
+// const path = require('path');
+
+const express = require('express')
+
+var app = express();
+
+
+app.use("/", express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/index.ejs');
+});
+
+var httpServer = http.createServer(app);
+
+httpServer.listen(3000);
+
+console.log('Started Server!')
