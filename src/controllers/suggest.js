af8da33227bd74dd0eb2ab62d2cafea052f37f78
Ivan <iFang.py@gmail.com>
2023-07-01 18:17:21 +1000

    E, changed for user inputs in suggest

--- src/controllers/suggest.js
+++ src/controllers/suggest.js
@@ -6,8 +6,8 @@ const { exec } = require('node:child_process')
 // Calculating places based of genres and lists of places
 function suggest(genre, placeArr, eventsArr) {
     // Match genre to types of place
-    places = exec('"/places.py"');
-    events = exec('"/events.py"');
+    places = exec(`python /places.py arg1 arg2 arg3 arg4`);
+    events = exec(`python /events.py arg1 arg2`);
     return {
         places: places,
         events: events,
